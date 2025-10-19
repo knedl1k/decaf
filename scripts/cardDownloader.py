3d5db86d6e02d4a57e1bfab6cf487a5abd14a003
knedl1k <knedl1k@tuta.io>
2025-10-19 15:15:15 +0200

    cardDownloader: handle cards with multiple artworks

--- scripts/cardDownloader.py
+++ scripts/cardDownloader.py
@@ -51,27 +51,43 @@ def parse():
     source_json = args.file
     return None
 
+def downloadImage(image, name, id):
+    try:
+        wgetDownload(image, output_path=f"{DIR_NAME}/{name}-{id}.png")
+    except Exception as e:
+        log.warning(id+": is sus-"+str(e))
+    return None          
+
 def main():
     with open(source_json) as f:
         d = json.load(f)
         n = len(d)
-        for i in range(50): # TODO: change range
+        for i in range(500): # TODO: change range
             id = d[i]['id']
-            if i%50 == 0: # naively try not to get banned
-                time.sleep(random.randint(SLEEP_MIN, SLEEP_MAX))
-            
-            di = Path(DIR_NAME)
-            if any(id in file.name for file in di.iterdir() if file.is_file()): # already downloaded
+
+            if any(id in file.name for file in Path(DIR_NAME).iterdir() if file.is_file()): # already downloaded
                 print(f"{id} already present!")
                 continue
 
+            if i%50 == 0: # naively try not to get banned
+                time.sleep(random.randint(SLEEP_MIN, SLEEP_MAX))
+
             name = d[i]['scryfall_uri'] \
             .replace('https://scryfall.com/card/', '').replace('?utm_source=api', '').replace("/", "_")
-            try:
-                image = d[i]['image_uris']['png']
-                wgetDownload(image, output_path=f"{DIR_NAME}/{name}-{id}.png")
-            except Exception as e:
-                log.warning(id+": is sus-"+str(e))
+
+            if d[i]['layout'] in ["transform", "modal_dfc"]: # has two sides, we need both # "art_series",
+                faces = d[i]['card_faces']
+                for j in range(0, 2, 1):
+                    side = faces[j]
+                    try:
+                        downloadImage(side['image_uris']['png'], name, id + "_" +("back" if j else "front"))
+                    except Exception as e:
+                        log.warning(id+": is sus- "+str(e))
+            else:
+                try:
+                    downloadImage(d[i]['image_uris']['png'], name, id)
+                except Exception as e:
+                    log.warning(id+": is sus- "+str(e))
     return None
 
 if __name__ == "__main__":
