339e2b8902f6814ca03bebc370b697bd25b7a2a1
knedl1k <knedl1k@tuta.io>
2026-01-05 11:49:38 +0100

    address changes in img_scraper.py

--- scripts/job_img_scraper.sh
+++ scripts/job_img_scraper.sh
@@ -3,12 +3,13 @@
 #SBATCH --nodes=1                   # 1 node
 #SBATCH --ntasks-per-node=1         # 1 tasks per node
 #SBATCH --time=24:00:00             # time limits: 24 hours
-#SBATCH --error=cardDownloader.err    # standard error file
-#SBATCH --output=cardDownloader.out   # standard output file
+#SBATCH --error=scraper.err    # standard error file
+#SBATCH --output=scraper.out   # standard output file
 #SBATCH --partition=cpuextralong    # partition name
 #SBATCH --mem=10G
 #SBATCH --mail-user=adamej14@fel.cvut.cz # where send info about job
-#SBATCH --mail-type=ALL             # what to send, valid type values are NONE, BEGIN, END, FAIL, REQUEUE, ALL
+#SBATCH --mail-type=END             # what to send, valid type values are NONE, BEGIN, END, FAIL, REQUEUE, ALL
 
 ml Python/3.13.5-GCCcore-14.3.0
-srun python3 cardDownloader.py -f all-cards-20251018215107.json -s /mnt/personal/adamej14/images
+ml Python-bundle-PyPI/2025.07-GCCcore-14.3.0
+srun python3 img_scraper.py -f default-cards-20260105101020.json -s /mnt/personal/adamej14/dataset
